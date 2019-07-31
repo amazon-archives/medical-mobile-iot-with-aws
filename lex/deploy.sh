33ff7a5d550f894731d0cad715e6e19773c7b5f2
jasonrichardsmith <jasonrichardsmith@gmail.com>
2019-07-31 10:34:35 +0200

    removing guardiuan angel refrences

--- lex/deploy.sh
+++ lex/deploy.sh
@@ -2,4 +2,4 @@
 REGION=us-east-1
 aws iam create-service-linked-role --aws-service-name lex.amazonaws.com
 aws lex-models put-intent --region ${REGION} --name medical-mobile-iot-with-aws-intent --cli-input-json file://medical-mobile-iot-with-aws-intent.json
-aws lex-models put-bot --region ${REGION} --name GuardianAngel --cli-input-json file://medical-mobile-iot-with-aws-bot.json
+aws lex-models put-bot --region ${REGION} --name medical-mobile-iot-with-aws-bot --cli-input-json file://medical-mobile-iot-with-aws-bot.json
